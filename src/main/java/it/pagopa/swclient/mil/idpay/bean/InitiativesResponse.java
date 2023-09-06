7535137a1f5706b2f26a1222b2109126e1d66079
EBOBROAFF <alexxander.bobrov@nttdata.com>
2023-09-06 15:46:21 +0200

    @RegisterForReflection added for response beans

--- src/main/java/it/pagopa/swclient/mil/idpay/bean/InitiativesResponse.java
+++ src/main/java/it/pagopa/swclient/mil/idpay/bean/InitiativesResponse.java
@@ -1,6 +1,7 @@
 package it.pagopa.swclient.mil.idpay.bean;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@RegisterForReflection
 public class InitiativesResponse {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
