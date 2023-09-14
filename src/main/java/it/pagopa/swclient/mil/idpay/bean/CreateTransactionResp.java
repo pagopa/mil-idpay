915bff7d080c14653a088b5c3b59348c42e81e94
EBOBROAFF <alexxander.bobrov@nttdata.com>
2023-09-14 10:52:41 +0200

    Code duplication fixed

--- src/main/java/it/pagopa/swclient/mil/idpay/bean/CreateTransactionResp.java
+++ src/main/java/it/pagopa/swclient/mil/idpay/bean/CreateTransactionResp.java
@@ -12,40 +12,12 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @RegisterForReflection
-public class CreateTransactionResp {
-
-    @NotNull
-    @Pattern(regexp = "^[\u0001-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]{1,256}$")
-    private String idpayTransactionId;
-
-    @NotNull
-    @Pattern(regexp = "^[\u0001-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]{1,256}$")
-    private String milTransactionId;
-
-    @NotNull
-    @Pattern(regexp = "^[\u0001-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]{1,256}$")
-    private String initiativeId;
-
-    @NotNull
-    @Pattern(regexp = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[0-1])T(2[0-3]|[01]\\d):[0-5]\\d:[0-5]\\d")
-    private String timestamp;
-
-    @NotNull
-    @Min(value = 0L)
-    @Max(value = 99999999999L)
-    private Long goodsCost;
+public class CreateTransactionResp extends Transaction{
 
     @Size(min = 8, max = 16)
     private byte[] challenge;
 
     @NotNull
     @Pattern(regexp = "^[\u0001-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]{1,256}$")
-    private String trxCode;
-
-    @NotNull
-    @Pattern(regexp = "^[\u0001-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]{1,256}$")
     private String qrCode;
-
-    @NotNull
-    private TransactionStatus status;
 }
