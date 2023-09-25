632f7e767522bea06af53d0d3143738e8c8a7789
EBOBROAFF <alexxander.bobrov@nttdata.com>
2023-09-25 10:26:05 +0200

    Idpay calls header changed

--- src/main/java/it/pagopa/swclient/mil/idpay/client/IdpayTransactionsRestClient.java
+++ src/main/java/it/pagopa/swclient/mil/idpay/client/IdpayTransactionsRestClient.java
@@ -15,15 +15,15 @@ public interface IdpayTransactionsRestClient {
     @POST
     @Path("")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<TransactionResponse> createTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String xMerchantFiscalcode, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, TransactionCreationRequest transactionCreationRequest);
+    Uni<TransactionResponse> createTransaction(@HeaderParam("x-merchant-id") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, TransactionCreationRequest transactionCreationRequest);
 
     @GET
     @Path("/status/{transactionId}")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<SyncTrxStatus> getStatusTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String xMerchantFiscalcode, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
+    Uni<SyncTrxStatus> getStatusTransaction(@HeaderParam("x-merchant-id") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
 
     @DELETE
     @Path("/{transactionId}")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<Void> deleteTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String xMerchantFiscalcode, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
+    Uni<Void> deleteTransaction(@HeaderParam("x-merchant-id") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
 }
