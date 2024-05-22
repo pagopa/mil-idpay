0f6190f8f566ca125e4ad580e9501ea381c65b0f
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2024-05-22 09:57:11 +0200

    Modified fields with cents according to idpay

--- src/main/java/it/pagopa/swclient/mil/idpay/client/IdpayRestClient.java
+++ src/main/java/it/pagopa/swclient/mil/idpay/client/IdpayRestClient.java
@@ -21,7 +21,9 @@ public interface IdpayRestClient {
     @GET
     @Path("/idpay/mil/merchant/initiatives")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<List<InitiativeDTO>> getMerchantInitiativeList(@HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId);
+    Uni<List<InitiativeDTO>> getMerchantInitiativeList(
+            @HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId,
+            @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId);
 
     /*
      * TRANSACTIONS
@@ -29,22 +31,34 @@ public interface IdpayRestClient {
     @POST
     @Path("/idpay/mil/payment")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<TransactionResponse> createTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, TransactionCreationRequest transactionCreationRequest);
+    Uni<TransactionResponse> createTransaction(
+            @HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId,
+            @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId,
+            TransactionCreationRequest transactionCreationRequest);
 
     @GET
     @Path("/idpay/mil/payment/{transactionId}/status")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<SyncTrxStatus> getStatusTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
+    Uni<SyncTrxStatus> getStatusTransaction(
+            @HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId,
+            @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId,
+            @PathParam("transactionId") String transactionId);
 
     @PUT
     @Path("/idpay/mil/payment/idpay-code/{transactionId}/preview")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<PreAuthPaymentResponseDTO> putPreviewPreAuthPayment(@HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
+    Uni<PreAuthPaymentResponseDTO> putPreviewPreAuthPayment(
+            @HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId,
+            @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId,
+            @PathParam("transactionId") String transactionId);
 
     @DELETE
     @Path("/idpay/mil/payment/{transactionId}")
     @ClientHeaderParam(name = "Ocp-Apim-Subscription-Key", value = "${idpay-rest-client.apim-subscription-key}", required = false)
-    Uni<Void> deleteTransaction(@HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId, @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId, @PathParam("transactionId") String transactionId);
+    Uni<Void> deleteTransaction(
+            @HeaderParam("x-merchant-fiscalcode") @NotNull String idpayMerchantId,
+            @HeaderParam("x-acquirer-id") @NotNull String xAcquirerId,
+            @PathParam("transactionId") String transactionId);
 
     /*
      * AUTHORIZE TRANSACTIONS
